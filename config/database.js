baa526b580e0115d4e4c2623e25a4c19c31bd48f
unknown <mimi.raykova@gmail.com>
2022-02-01 22:57:25 +0200

    change database settings

--- config/database.js
+++ config/database.js
@@ -11,25 +11,38 @@
 //     },
 //   },
 // });
-
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env(
-        "DATABASE_HOST",
-        "ec2-52-2-245-64.compute-1.amazonaws.com"
-      ),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "den2vt2drdffn6"),
-      user: env("DATABASE_USERNAME", "qdribgsqzoxkso"),
-      password: env(
-        "DATABASE_PASSWORD",
-        "3746bea71e38ccc05fc24d7b29d8c23bf988b7f47b982c10e114b47d647346ba"
-      ),
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
       // ssl: env.bool('DATABASE_SSL', true), //тук смених със следното, иначе излизаше грешка
       ssl: { rejectUnauthorized: false },
     }
   }
 });
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env(
+//         "DATABASE_HOST",
+//         "ec2-52-2-245-64.compute-1.amazonaws.com"
+//       ),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "den2vt2drdffn6"),
+//       user: env("DATABASE_USERNAME", "qdribgsqzoxkso"),
+//       password: env(
+//         "DATABASE_PASSWORD",
+//         "3746bea71e38ccc05fc24d7b29d8c23bf988b7f47b982c10e114b47d647346ba"
+//       ),
+//       // ssl: env.bool('DATABASE_SSL', true), //тук смених със следното, иначе излизаше грешка
+//       ssl: { rejectUnauthorized: false },
+//     }
+//   }
+// });
 
